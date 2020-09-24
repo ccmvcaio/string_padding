0246d9af0bca8787882c04c626f09f977c457fe4
Caio César Valério <ccmvcaio@gmail.com>
2020-09-24 18:34:11 -0300

    Validatin padding method
    Co-authored-by: Paulo Rodrigues <paulor2222@gmail.com

--- lib/string_utils.rb
+++ lib/string_utils.rb
@@ -1,5 +1,10 @@
 class StringUtils
   def padding(frase, padding_array)
-
+    return frase if padding_array == []
+    upper = "\n" * padding_array[0]
+    right = " " * padding_array[1]
+    bottom = "\n" * padding_array[2]
+    left = " " * padding_array[3]
+    "#{upper}#{right}#{frase}#{left}#{bottom}"
   end
 end
--- spec/string_utils_spec.rb
+++ spec/string_utils_spec.rb
@@ -9,4 +9,30 @@ describe StringUtils do
                   "\n"
     expect(StringUtils.new.padding(frase, [1, 1, 1, 1])).to eq expectativa
   end
+
+  it 'Checa espaços maiores que 1' do
+    frase = 'Telefone... Minha casa.'
+
+    expectativa = "\n\n"\
+                  "  Telefone... Minha casa.  "\
+                  "\n\n"
+    
+    expect(StringUtils.new.padding(frase, [2, 2, 2, 2])).to eq expectativa
+  end
+  
+  it 'Checa espaços diferentes' do
+    frase = 'Telefone... Minha casa.'
+
+    expectativa = "\n"\
+                  "  Telefone... Minha casa.   "
+                  "\n\n"
+
+    expect(StringUtils.new.padding(frase, [1, 2, 3, 2]))
+  end
+
+  it 'Checa [] array vazio' do
+    frase = 'Telefone... Minha casa.'
+
+    expect(StringUtils.new.padding(frase, [])).to eq frase
+  end
 end
